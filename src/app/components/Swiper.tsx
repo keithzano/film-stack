6e040df4118a68878c2b33ec31c11cb3303c2398
keithzano <keithzano>
2024-02-10 01:11:08 +0200

    Fixed the swiper centering it and removing overflow

--- src/app/components/Swiper.tsx
+++ src/app/components/Swiper.tsx
@@ -6,7 +6,7 @@ import { MovieInList } from "../utils/request";
 export const Swiper = ({ movies }: { movies: MovieInList[] }) => {
   const [emblaRef] = useEmblaCarousel();
   return (
-    <div className="  " ref={emblaRef}>
+    <div className=" max-w-screen-xl overflow-x-hidden mx-auto " ref={emblaRef}>
       <div className="  flex gap-6">
         {movies.map((movie) => (
           <MovieCard key={movie.id} movie={movie} />
